77be2db501980593b34ce0a358cab057dd0935a8
jadeola <jadeola@unomaha.edu>
2017-09-13 21:59:49 -0500

    Modified login to use userName instead of email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,15 +10,15 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('userName') ? ' has-error' : '' }}">
+                            <label for="userName" class="col-md-4 control-label">User Name</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="userName" type="userName" class="form-control" name="userName" value="{{ old('userName') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('userName'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('userName') }}</strong>
                                     </span>
                                 @endif
                             </div>
